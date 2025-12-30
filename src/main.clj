2fd6b7cac6e9ab8483745feae149862ed44e5c7a
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-12-30 23:05:37 +0530

    Adds hiccup module to replace html string to keywords

--- src/main.clj
+++ src/main.clj
@@ -1,20 +1,22 @@
 (ns main
 
   (:require
-   [org.httpkit.server :as server]))
+   [org.httpkit.server :as server]
+   [hiccup.core :refer [html]]))
+
+(defn page []
+  (html
+   [:html
+    [:body
+     [:h1 "Hello world!"]
+     [:p "This is a simple web page served using http-kit and rendered with Hiccup."]]]))
 
 (defn handler [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello, again</h1>
-          <h2>Heading 2</h2>
-          <p>This is a paragraph.</p>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>"})
+   :body (page)})
 
 (defn -main []
   (server/run-server handler {:port 8000})
   (println "Server running at http://localhost:8000"))
+
